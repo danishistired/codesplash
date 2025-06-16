d3971ce2fa320bd9d6145f1d8b0eb940fd6260c2
Danish <IamDanishVerma@gmail.com>
2025-06-16 23:27:18 +0530

    got the base extension to work

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,104 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const provider = new CodeShareViewProvider(context.extensionUri);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codesplash" is now active!');
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      'codeShareView',
+      provider
+    )
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('codesplash.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Code Splash!');
-	});
+  context.subscriptions.push(
+    vscode.commands.registerCommand('codeShare.sendCode', async () => {
+      vscode.window.showInformationMessage('Send Code button clicked!');
+    })
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('codeShare.receiveCode', async () => {
+      const input = await vscode.window.showInputBox({
+        prompt: 'Enter the code share link'
+      });
+
+      if (input) {
+        vscode.window.showInformationMessage(`Received link: ${input}`);
+      }
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
+class CodeShareViewProvider implements vscode.WebviewViewProvider {
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    webviewView.webview.options = {
+      enableScripts: true
+    };
+
+    webviewView.webview.html = this._getHtml(webviewView.webview);
+    webviewView.webview.onDidReceiveMessage(async (data) => {
+      if (data.command === 'send') {
+        vscode.commands.executeCommand('codeShare.sendCode');
+      }
+      if (data.command === 'receive') {
+        vscode.commands.executeCommand('codeShare.receiveCode');
+      }
+    });
+  }
+
+  private _getHtml(webview: vscode.Webview): string {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <style>
+        body {
+          font-family: sans-serif;
+          padding: 10px;
+        }
+
+        button {
+          background-color:rgb(127, 137, 145);
+          color: white;
+          border: none;
+          padding: 8px 13px;
+          margin: 5px 0;
+          border-radius: 4px;
+          cursor: pointer;
+          width: 100%;
+          font-size: 11.5px;
+        }
+
+        button:hover {
+          background-color: #005a9e;
+        }
+
+        .description {
+          margin-bottom: 10px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="description">Splash your code to others to set up the environment automatically.</div>
+      <button onclick="sendCommand('send')">Send Code</button>
+      <button onclick="sendCommand('receive')">Receive Code</button>
+
+      <script>
+        const vscode = acquireVsCodeApi();
+        function sendCommand(command) {
+          vscode.postMessage({ command });
+        }
+      </script>
+    </body>
+    </html>
+  `;
+  }
+}
+
 export function deactivate() {}
